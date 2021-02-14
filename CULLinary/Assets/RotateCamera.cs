7c4587b8a326e2e5fcaa7b6e33383d56d9cf54d6
Darren Sim <klassiclegend@gmail.com>
2021-02-14 22:03:18 +0800

    Test out cinemachine

---
+++ CULLinary/Assets/RotateCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //transform.Rotate(Vector3.up, mouseX);
+    }
+
+}
--- CULLinary/Assets/Test/TestScripts/PlayerController.cs
+++ CULLinary/Assets/Test/TestScripts/PlayerController.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float rotSpeed;
+    
+    
+    private Vector3 moveDirection;
+    private Vector3 rotateDirection;
+    private Vector3 velocity;
+    private CharacterController controller;
+    private Animator animator;
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        moveDirection = new Vector3(0.0f, 0.0f, moveVertical);
+        rotateDirection = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (isGrounded)
+        {
+            if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+            else
+            {
+                Walk();
+            }
+
+            moveDirection *= moveSpeed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        //Quaternion toRotation = Quaternion.LookRotation(rotateDirection, Vector3.up);
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotSpeed * Time.deltaTime);
+        transform.Rotate(0, moveHorizontal * rotSpeed * Time.deltaTime, 0);
+    }
+
+    private void Idle()
+    {
+        animator.SetFloat("Speed", 0.0f, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        animator.SetFloat("Speed", 1.0f, 0.1f, Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
     }
 }
