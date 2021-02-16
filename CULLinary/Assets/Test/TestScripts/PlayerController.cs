0d0435191cd38a7710132a4b75706ee7fa57bc96
Darren Sim <klassiclegend@gmail.com>
2021-02-16 09:12:08 +0800

    Test cinemachine with aim down sights

--- CULLinary/Assets/Test/TestScripts/PlayerController.cs
+++ CULLinary/Assets/Test/TestScripts/PlayerController.cs
@@ -1,24 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Speed
     [SerializeField] private float walkSpeed;
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float runSpeed;
+
+    //Check ground
     [SerializeField] private bool isGrounded;
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float gravity;
+
+    //height
     [SerializeField] private float jumpHeight;
-    [SerializeField] private float rotSpeed;
-    
-    
+    [SerializeField] private float turnSmoothTime;
+
+
+    //Cameras
+    [SerializeField] private Transform cam; //Main camera
+    [SerializeField] private GameObject tpCam; //Third Person Camera
+    [SerializeField] private GameObject aimCam; //Aim Camera
+
+    private float moveSpeed = 1.0f;
+    private Vector3 direction;
+    private Vector3 normalizedDirection;
     private Vector3 moveDirection;
-    private Vector3 rotateDirection;
     private Vector3 velocity;
+    private float turnSmoothVelocity;
     private CharacterController controller;
     private Animator animator;
+    private bool isAttacking = false;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -27,52 +43,95 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        Aim();
+        Punch();
         Move();
+        
+    }
+
+    private void Punch()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            isAttacking = true;
+            animator.SetBool("isPunch", true);
+        } else
+        {
+            isAttacking = false;
+            animator.SetBool("isPunch", false);
+        }
+    }
+
+    private void Aim()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            isAttacking = true;
+            tpCam.SetActive(false);
+            aimCam.SetActive(true);
+            animator.SetBool("isAim", true);
+            
+        } else
+        {
+            isAttacking = false;
+            tpCam.SetActive(true);
+            aimCam.SetActive(false);
+            animator.SetBool("isAim", false);
+        }
     }
 
     private void Move()
     {
+        //Get input
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        normalizedDirection = direction.normalized;
+
+        float targetAngle = Mathf.Atan2(normalizedDirection.x, normalizedDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+        moveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+
         isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
-        float moveVertical = Input.GetAxis("Vertical");
-        float moveHorizontal = Input.GetAxis("Horizontal");
 
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
-        moveDirection = new Vector3(0.0f, 0.0f, moveVertical);
-        rotateDirection = new Vector3(moveHorizontal, 0.0f, 0.0f);
-        moveDirection = transform.TransformDirection(moveDirection);
-
-        if (isGrounded)
+        if (isGrounded && !isAttacking)
         {
-            if (moveDirection == Vector3.zero)
+            if (direction == Vector3.zero)
             {
                 Idle();
             }
             else
             {
-                Walk();
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    Run();
+                }
+                else
+                {
+                    Walk();
+                }
             }
-
-            moveDirection *= moveSpeed;
-
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();
             }
         }
 
-        controller.Move(moveDirection * Time.deltaTime);
+        //Rotation
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
+        //Gravity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
-        //Quaternion toRotation = Quaternion.LookRotation(rotateDirection, Vector3.up);
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotSpeed * Time.deltaTime);
-        transform.Rotate(0, moveHorizontal * rotSpeed * Time.deltaTime, 0);
     }
-
+    
     private void Idle()
     {
         animator.SetFloat("Speed", 0.0f, 0.1f, Time.deltaTime);
@@ -81,7 +140,15 @@ public class PlayerController : MonoBehaviour
     private void Walk()
     {
         moveSpeed = walkSpeed;
+        animator.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+        controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
         animator.SetFloat("Speed", 1.0f, 0.1f, Time.deltaTime);
+        controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
     }
 
     private void Jump()
