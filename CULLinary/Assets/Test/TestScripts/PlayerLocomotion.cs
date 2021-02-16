a851c586a3bd86d8c7d52ec2cc335d357aa3259b
Darren Sim <klassiclegend@gmail.com>
2021-02-16 21:21:10 +0800

    Implement Accurate Aim

---
+++ CULLinary/Assets/Test/TestScripts/PlayerLocomotion.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private CharacterController controller;
+    private Vector3 direction;
+    private Vector3 normalizedDirection;
+    private Vector3 moveDirection;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        normalizedDirection = direction.normalized;
+
+    }
+}
