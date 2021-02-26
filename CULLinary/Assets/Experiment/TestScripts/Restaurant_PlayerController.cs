556235f91af689c1cb875223c08e4bafc1e63fd0
alcen <49450743+alcen@users.noreply.github.com>
2021-02-26 12:02:33 +0800

    Restaurant camera and control tweaks
    * Fix camera to focus on player
    
    * Adjusted camera angle
    
    * Add more movement optiosn for player
    
    * Expand navmesh area to larger restaurant size

--- CULLinary/Assets/Experiment/TestScripts/Restaurant_PlayerController.cs
+++ CULLinary/Assets/Experiment/TestScripts/Restaurant_PlayerController.cs
@@ -4,14 +4,23 @@ using UnityEngine.AI;
 public class Restaurant_PlayerController : MonoBehaviour
 {
     public Camera cam;
-
     public NavMeshAgent agent;
+    public float wasdAvoidanceRadius = 0.0f;
+    
+    bool keyInput = false;
+    float defaultRadius;
+
+    void Start() {
+        defaultRadius = agent.radius;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
+            agent.radius = defaultRadius;
+            keyInput = false;
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
@@ -21,6 +30,28 @@ public class Restaurant_PlayerController : MonoBehaviour
                 agent.SetDestination(hit.point);
                 Debug.Log("Moving the agent!");
             }
+        }
+
+        Vector3 movementOffset = new Vector3(0.0f, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.W)) {
+            keyInput = true;
+            movementOffset = movementOffset + new Vector3(0.0f, 0.0f, -5.0f);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            keyInput = true;
+            movementOffset = movementOffset + new Vector3(5.0f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            keyInput = true;
+            movementOffset = movementOffset + new Vector3(0.0f, 0.0f, 5.0f);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            keyInput = true;
+            movementOffset = movementOffset + new Vector3(-5.0f, 0.0f, 0.0f);
+        }
+        if (keyInput) {
+            agent.radius = wasdAvoidanceRadius;
+            agent.SetDestination(agent.transform.position + movementOffset);
         }
     }
 }
