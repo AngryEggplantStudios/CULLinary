18a330d6ed5596290d059dc32f6a970457c42389
WhiteLio <lio.lim@yahoo.com>
2021-03-05 21:29:11 +0800

    Added new Rest. Ext. Scene
    Fixed Chef Prefab
    Cleaned up unused tags

--- CULLinary/Assets/Experiment/TestAssets/Dungeon Scripts/DungeonPlayerAim.cs
+++ CULLinary/Assets/Experiment/TestAssets/Dungeon Scripts/DungeonPlayerAim.cs
@@ -19,7 +19,7 @@ public class DungeonPlayerAim : MonoBehaviour
 
     //Defaults
     private const float MAX_DIST_CAM_TO_GROUND = 100f;
-    private const float LINE_HEIGHT_FROM_GROUND = 0.2f;
+    private const float LINE_HEIGHT_FROM_GROUND = 0.1f;
 
     private Vector3 targetPosition = new Vector3();
     private Vector3 sourcePosition = new Vector3();
@@ -56,6 +56,7 @@ public class DungeonPlayerAim : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit, MAX_DIST_CAM_TO_GROUND, 1 << LayerMask.NameToLayer("Ground")))
         {
+            Debug.Log(this.lookVector + " " + transform.position);
             this.lookVector = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             this.sourcePosition = new Vector3(transform.position.x, LINE_HEIGHT_FROM_GROUND, transform.position.z);
             this.targetPosition = new Vector3(hit.point.x, LINE_HEIGHT_FROM_GROUND, hit.point.z);
@@ -69,7 +70,6 @@ public class DungeonPlayerAim : MonoBehaviour
         {
             targetFound = false;
         }
-
     }
 
     private void Update()
