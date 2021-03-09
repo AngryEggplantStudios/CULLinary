1c96d82552260413c28b4ef0c80b416064a16679
geezlouisee <louisehl.lee@gmail.com>
2021-03-09 14:43:04 +0800

    Changed Locomotion
    Changed player controller for restaurant (KIV: anims prob need to be more in sync w the walking movement)

---
+++ CULLinary/Assets/Experiment/TestScripts/Restaurant Scripts/Restaurant_newPlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Restaurant_newPlayerController : MonoBehaviour
+{
+    public Camera cam;   
+    public Animator animator;
+        
+    public ServingController servingController;
+
+    // For Player Movement
+    public CharacterController controller;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public float speed = 10.0f;
+    private Vector3 direction;
+    private Vector3 moveDirection;
+
+
+    private void Update()
+    {
+        //Get input
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        //Calculations
+        direction = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            moveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
+        // Handle animations
+        animator.SetBool("isWalking", (moveVertical != 0.0f || moveHorizontal != 0.0f) );
+        animator.SetBool("hasFood", servingController.holdingItem);
+    }
+}
