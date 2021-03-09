b7d5eae3a8df9689a76689bc5a2664e45055f2fe
alcen <49450743+alcen@users.noreply.github.com>
2021-03-09 19:25:45 +0800

    Add UI menu for recipes

---
+++ CULLinary/Assets/Experiment/TestScripts/Restaurant Scripts/UiButtonClose.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiButtonClose : MonoBehaviour
+{
+    public GameObject ui;
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.collider != null && hit.collider.gameObject.tag == "CloseButton")
+            {
+                ui.SetActive(false);
+            }
+        }
+    }
+}
