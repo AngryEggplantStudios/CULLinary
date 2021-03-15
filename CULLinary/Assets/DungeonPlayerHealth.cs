6c609c054ab93af6291580264a8be485850681ac
MC <dandford@gmail.com>
2021-03-15 21:04:45 +0800

    Fix monster AI bugs in procedure Map + navmesh

--- CULLinary/Assets/DungeonPlayerHealth.cs
+++ CULLinary/Assets/DungeonPlayerHealth.cs
@@ -78,7 +78,7 @@ public class DungeonPlayerHealth : MonoBehaviour
         Vector3 forceDirection = transform.position - positionOfEnemy;
         forceDirection.y = 0;
         Vector3 force = forceDirection.normalized;
-        dpl.KnockBack(force, 50, 3, true);
+        //dpl.KnockBack(force, 50, 3, true);
     }
 
     private IEnumerator KnockCoroutine(Vector3 positionOfEnemy)
