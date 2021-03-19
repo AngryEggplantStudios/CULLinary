7ce3c70e6f1bd28b3dc489dde97e4902f1fe6e23
MC <dandford@gmail.com>
2021-03-19 11:41:31 +0800

    Update corn to be ranged attack

--- CULLinary/Assets/CornAttack.cs
+++ CULLinary/Assets/CornAttack.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class CornAttack : EnemyAttack
 {
-    private GameObject attackRadius;
+    [SerializeField] private Transform throwObject;
     private SphereCollider attackCollider;
     private DungeonPlayerHealth healthScript;
     private bool canDealDamage;
-
+    private Transform player; 
 
     private void Awake()
     {
 
         attackCollider = gameObject.GetComponent<SphereCollider>();
         canDealDamage = false;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void throwCorn(Vector3 sourcePosition, Vector3 targetPosition)
+    {
+        Transform cornTransform = Instantiate(throwObject, sourcePosition, Quaternion.identity);
+        cornTransform.GetComponent<EnemyProjectile>().Setup(sourcePosition, targetPosition);
     }
 
     public override void attackPlayerStart()
@@ -22,12 +29,13 @@ public class CornAttack : EnemyAttack
         //this.selectionCircleActual = Instantiate(this.selectionCirclePrefab);
         //this.selectionCircleActual.transform.SetParent(this.transform, false);
         //this.selectionCircleActual.transform.eulerAngles = new Vector3(90, 0, 0);
-        attackCollider.enabled = true;
+        //attackCollider.enabled = true;
     }
 
     public override void attackPlayerDealDamage()
     {
         canDealDamage = true;
+        throwCorn(transform.position, player.transform.position);
     }
 
 
---
+++ CULLinary/Assets/EnemyProjectile.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    private Vector3 projDir;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float damage;
+    [SerializeField] private float heightFromGround;
+
+    private float spinSpeed = -1500;
+
+    public void Setup(Vector3 sourcePosition, Vector3 targetPosition)
+    {
+        this.projDir = (targetPosition - sourcePosition).normalized;
+        transform.position += new Vector3(0, heightFromGround, 0);
+        transform.eulerAngles = new Vector3(0, CalculateAngle(projDir), 90);
+        Destroy(gameObject, 5);
+    }
+
+    private float CalculateAngle(Vector3 v)
+    {
+        v = v.normalized;
+        float n = Mathf.Atan2(v.x, v.z) * Mathf.Rad2Deg;
+        if (n < 0)
+        {
+            n += 360;
+        }
+        return n;
+    }
+
+    private float CalculateDuration(Vector3 sourcePosition, Vector3 targetPosition)
+    {
+        return Vector3.Distance(sourcePosition, targetPosition) / moveSpeed;
+    }
+
+    private void Update()
+    {
+        transform.position += projDir * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DungeonPlayerHealth target = other.GetComponent<DungeonPlayerHealth>();
+        if (target != null)
+        {
+            target.HandleHit(damage);
+            Destroy(gameObject);
+            Debug.Log("Hit!");
+        } else
+        {
+            GameObject target2 = other.transform.Find("AttackRadius").gameObject;
+            if (target2 == null)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
