a438578032da8d805affd8baed476d932d5a24af
Darren Sim <klassiclegend@gmail.com>
2021-03-28 16:07:59 +0800

    Integrate Lio rooms

--- CULLinary/Assets/Experiment/TestScripts/Generator Scripts/ConnectionPoint.cs
+++ CULLinary/Assets/Experiment/TestScripts/Generator Scripts/ConnectionPoint.cs
@@ -6,6 +6,110 @@ using UnityEditor;
 public class ConnectionPoint : MonoBehaviour
 {
     [SerializeField] private bool isConnected = false;
+    [SerializeField] private GameObject[] spawnRooms;
+    [SerializeField] private GameObject deadend;
+    [SerializeField] private float bias; //How far from the centre should the connection point spawn the next room?
+
+    public IEnumerator GenerateRoom()
+    {
+        int generatedProb = Random.Range(0, spawnRooms.Length);
+        GameObject roomToGenerate = spawnRooms[generatedProb];
+        Quaternion rotation = Quaternion.LookRotation(this.transform.forward);
+        Vector3 position = GenerateRelativeVector(rotation.eulerAngles.y, bias);
+        GameObject generatedRoom = Instantiate(roomToGenerate, position, rotation);
+        yield return null;
+        ConnectionPoint[] ConnectionPoints = generatedRoom.GetComponentsInChildren<ConnectionPoint>();
+        CheckCollision validatorNewRoom = generatedRoom.GetComponentInChildren<CheckCollision>();
+        ConnectionPoint chosenPoint = ConnectionPoints[0];
+        validatorNewRoom.TurnOnCollider();
+        yield return new WaitForSeconds(0.05f);
+        if (validatorNewRoom.GetIsCollided())
+        {
+            Destroy(generatedRoom);
+            yield return null;
+        }
+        else
+        {
+            MapGenerator.AddGeneratedRoom(generatedRoom);
+            this.SetConnected();
+            chosenPoint.SetConnected();
+            MapGenerator.AddConnectionPoints(ConnectionPoints);
+        }
+    }
+
+    public IEnumerator GenerateDeadend()
+    {
+        GameObject roomToGenerate = deadend;
+        Quaternion rotation = Quaternion.LookRotation(this.transform.forward);
+        Vector3 position = GenerateRelativeVector(rotation.eulerAngles.y, bias);
+        GameObject generatedRoom = Instantiate(roomToGenerate, position, rotation);
+        yield return null;
+        CheckCollision validatorNewRoom = generatedRoom.GetComponentInChildren<CheckCollision>();
+        validatorNewRoom.TurnOnCollider();
+        yield return new WaitForSeconds(0.05f);
+        if (validatorNewRoom.GetIsCollided())
+        {
+            Destroy(generatedRoom);
+            yield return null;
+        }
+        else
+        {
+            this.SetConnected();
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Validator"))
+        {
+            isConnected = true;
+        }
+    }
+
+    public Vector3 GenerateRelativeVector(float eulerAngle, float bias)
+    {
+        float xBias = 0f;
+        float zBias = 0f;
+        if (eulerAngle > -10.0f && eulerAngle < 10.0f)
+        {
+            zBias = bias;
+        }
+        else if (eulerAngle > 80.0f && eulerAngle < 100.0f)
+        {
+            xBias = bias;
+        }
+        else if (eulerAngle > 170.0f && eulerAngle < 190.0f)
+        {
+            zBias = -bias;
+        }
+        else if (eulerAngle > 260.0f && eulerAngle < 280.0f)
+        {
+            xBias = -bias;
+        }
+        return new Vector3(this.transform.position.x + xBias, this.transform.position.y, this.transform.position.z + zBias);
+    }
+
+    public void SetConnected()
+    {
+        isConnected = true;
+    }
+
+    public bool GetIsConnected()
+    {
+        return isConnected;
+    }
+
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ConnectionPoint : MonoBehaviour
+{
+    [SerializeField] private bool isConnected = false;
 
     [System.Serializable]
     private class SpawnRoom
@@ -171,3 +275,4 @@ public class ConnectionPoint : MonoBehaviour
     }
 
 }
+*/
--- CULLinary/Assets/Experiment/TestScripts/Generator Scripts/MapGenerator.cs
+++ CULLinary/Assets/Experiment/TestScripts/Generator Scripts/MapGenerator.cs
@@ -6,6 +6,106 @@ using UnityEngine.AI;
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject startingRoom;
+    [SerializeField] private int roomLimit;
+
+    private static GameObject parent;
+    private static int roomCounter = 0;
+    private static List<GameObject> generatedRooms = new List<GameObject>();
+    
+    private static Queue<ConnectionPoint> ConnectionPoints = new Queue<ConnectionPoint>();
+
+    /*
+    For Loading Screen usage
+    */
+    public static bool isGenerated = false;
+    public static float roomProgress = 0f;
+    public static bool isGeneratingRooms = true;
+    public static bool isBuildingNavMesh = false;
+    public static MapGenerator current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    private void Start()
+    {
+        parent = new GameObject();
+        parent.AddComponent<NavMeshSurface>();
+        generatedRooms = new List<GameObject>();
+        ConnectionPoints = new Queue<ConnectionPoint>();
+
+        roomCounter = 0;
+        roomProgress = 0f;
+        isGeneratingRooms = true;
+        isBuildingNavMesh = false;
+        isGenerated = false;
+
+        StartCoroutine(GenerateMap());
+    }
+
+    private IEnumerator GenerateMap()
+    {
+        ConnectionPoint[] startingPoints = startingRoom.GetComponentsInChildren<ConnectionPoint>();
+        //Enqueues all the starting points' connection points
+        foreach (ConnectionPoint c in startingPoints)
+        {
+            ConnectionPoints.Enqueue(c);
+        }
+        yield return null;
+        //While we haven't exceed the room limit, let's try to generate the room for each connection point
+        while (ConnectionPoints.Count > 0 && roomCounter < roomLimit )
+        {
+            ConnectionPoint currentPoint = ConnectionPoints.Dequeue();
+            yield return null;
+            yield return StartCoroutine(currentPoint.GenerateRoom());
+        }
+        //For all the connection points left, let us generate the deadend
+        foreach (ConnectionPoint t in ConnectionPoints)
+        {
+            yield return null;
+            yield return StartCoroutine(t.GenerateDeadend());
+        }
+        isGeneratingRooms = false;
+        isBuildingNavMesh = true;
+        yield return new WaitForSeconds(0.05f);
+        //Let us build the navmesh now for the AI
+        parent.GetComponent<NavMeshSurface>().BuildNavMesh();
+        yield return new WaitForSeconds(0.05f);
+        isBuildingNavMesh = false;
+        isGenerated = true;
+    }
+
+    public static void AddConnectionPoints(ConnectionPoint[] points)
+    {
+        foreach (ConnectionPoint c in points)
+        {
+            if (!c.GetIsConnected())
+            {
+                ConnectionPoints.Enqueue(c);
+            }
+        }
+    }
+
+    public static void AddGeneratedRoom(GameObject room)
+    {
+        roomProgress += 1f / current.roomLimit;
+        roomCounter++;
+        generatedRooms.Add(room);
+        room.transform.parent = parent.transform;
+    }
+    
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject startingRoom;
 
     [SerializeField] private bool limitByRooms;
     [SerializeField] private int roomLimit;
@@ -16,9 +116,9 @@ public class MapGenerator : MonoBehaviour
     
     private static Queue<ConnectionPoint> connectionPoints = new Queue<ConnectionPoint>();
 
-    /*
+    
     For Loading Screen usage
-    */
+    
     public static bool isGenerated = false;
     public static float roomProgress = 0f;
     public static bool isGeneratingRooms = true;
@@ -113,3 +213,4 @@ public class MapGenerator : MonoBehaviour
     }
 
 }
+*/
