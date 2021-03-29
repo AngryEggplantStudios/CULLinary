a1abc2aa4f4c318bfb85481b39cae2df8671e4ad
MC <dandford@gmail.com>
2021-03-30 00:39:03 +0800

    Boss Ranged

---
+++ CULLinary/Assets/BossRangedAttack.cs
@@ -0,0 +1,191 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRangedAttack : EnemyAttack
+{
+    [SerializeField] private Transform throwObject;
+    private SphereCollider attackCollider;
+    private DungeonPlayerHealth healthScript;
+    private bool canDealDamage;
+    private Transform player;
+    private bool projectAttack = true;
+    private float startingAngle;
+    private float fov = 90f;
+    private float viewDistance = 5f;
+    private List<LineRenderer> listOfRenderers;
+    private List<Vector3> firePositions;
+    private const float LINE_HEIGHT_FROM_GROUND = 0.1f;
+    private int rayCount = 5;
+
+    private void Awake()
+    {
+        attackCollider = gameObject.GetComponent<SphereCollider>();
+        canDealDamage = false;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Start()
+    {
+        listOfRenderers = new List<LineRenderer>();
+        for (int i = 0; i < rayCount; i++)
+        {
+            GameObject gameObjectChild = new GameObject();
+            gameObjectChild.transform.parent = gameObject.transform;
+            LineRenderer lRend = gameObjectChild.AddComponent<LineRenderer>();
+            lRend.positionCount = 2;
+            lRend.SetWidth(0.01f, 0.02f);
+            listOfRenderers.Add(lRend);
+        }
+    }
+    private Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+    private float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+
+    private void throwCorn(Vector3 sourcePosition, Vector3 targetPosition)
+    {
+        Transform cornTransform = Instantiate(throwObject, sourcePosition, Quaternion.identity);
+        cornTransform.GetComponent<EnemyProjectile>().Setup(sourcePosition, targetPosition);
+    }
+
+    private void Update()
+    {
+        if (projectAttack)
+        {
+            Vector3 playerDirection = (player.transform.position - transform.position).normalized;
+            float angleIncrease = fov / rayCount;
+            float angle = angleIncrease * 2 * -1;
+            int layerMask = 1 << 8;
+            Vector3 finalDirection;
+            firePositions = new List<Vector3>();
+            for (int i = 0; i < rayCount; i++)
+            {
+                finalDirection = Quaternion.Euler(0, angle, 0) * playerDirection;
+                finalDirection.y = LINE_HEIGHT_FROM_GROUND;
+                RaycastHit hit;
+                // Does the ray intersect any objects excluding the player layer
+                Vector3 sourcePosition;
+                Vector3 targetPosition;
+                sourcePosition = new Vector3(transform.position.x, LINE_HEIGHT_FROM_GROUND, transform.position.z);
+                if (Physics.Raycast(sourcePosition, finalDirection, out hit, viewDistance, layerMask))
+                {
+                    LineRenderer lRend = listOfRenderers[i];
+                    targetPosition = new Vector3(hit.point.x, LINE_HEIGHT_FROM_GROUND, hit.point.z);
+                    lRend.SetPosition(0, sourcePosition);
+                    lRend.SetPosition(1, targetPosition);
+                }
+                else
+                {
+                    LineRenderer lRend = listOfRenderers[i];
+                    targetPosition = finalDirection * viewDistance + sourcePosition;
+                    targetPosition.y = LINE_HEIGHT_FROM_GROUND;
+                    lRend.SetPosition(0, sourcePosition);
+                    lRend.SetPosition(1, targetPosition);
+                }
+                firePositions.Add(targetPosition);
+                angle += angleIncrease;
+            }
+        }
+
+        
+    }
+
+    private IEnumerator prepareToFire()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            for (int i = 0; i < listOfRenderers.Count; i++)
+            {
+                listOfRenderers[i].enabled = !(listOfRenderers[i].enabled);
+            }
+        }
+
+    }
+
+    public override void attackPlayerStart()
+    {
+        //this.selectionCircleActual = Instantiate(this.selectionCirclePrefab);
+        //this.selectionCircleActual.transform.SetParent(this.transform, false);
+        //this.selectionCircleActual.transform.eulerAngles = new Vector3(90, 0, 0);
+        //attackCollider.enabled = true;
+        Debug.Log("Attack");
+        projectAttack = true;
+        for (int i = 0; i < listOfRenderers.Count; i++)
+        {
+            listOfRenderers[i].enabled = true;
+        }
+        StartCoroutine("prepareToFire");
+    }
+
+    public override void attackPlayerDealDamage()
+    {
+        projectAttack = false;
+        canDealDamage = true;
+        for (int i = 0; i < firePositions.Count; i++)
+        {
+            throwCorn(transform.position, firePositions[i]);
+        }
+
+    }
+
+
+    public override void attackPlayerEnd()
+    {
+        //Destroy(selectionCircleActual.gameObject);
+        attackCollider.enabled = false;
+        canDealDamage = false;
+        for (int i =  0; i < listOfRenderers.Count; i++)
+        {
+            listOfRenderers[i].enabled = false;
+        }
+        StopCoroutine("prepareToFire");
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (canDealDamage)
+        {
+
+            if (healthScript != null)
+            {
+                bool hitSuccess = healthScript.HandleHit(attackDamage);
+                if (hitSuccess)
+                {
+                    healthScript.KnockbackPlayer(transform.position);
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DungeonPlayerHealth target = other.GetComponent<DungeonPlayerHealth>();
+        if (target != null)
+        {
+            healthScript = target;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        DungeonPlayerHealth target = other.GetComponent<DungeonPlayerHealth>();
+        if (target != null)
+        {
+            healthScript = null;
+        }
+    }
+
+}
---
+++ CULLinary/Assets/BossScript.cs
@@ -0,0 +1,367 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class BossScript : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    private enum State
+    {
+        Roaming,
+        Idle,
+        ChaseTarget,
+        AttackTarget,
+        ShootingTarget,
+        GoingBackToStart,
+    }
+
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float distanceTriggered;
+    [SerializeField] private float stopChase;
+    [SerializeField] private float wanderTimer;
+    [SerializeField] private float idleTimer;
+    [SerializeField] private float timeBetweenAttacks;
+    [SerializeField] private float collideDamage;
+
+    [SerializeField] private GameObject hpBar_prefab;
+    private GameObject hpBar;
+    private Image hpBarFull;
+
+    [SerializeField] private GameObject damageCounter_prefab;
+    [SerializeField] private GameObject enemyAlert_prefab;
+    private List<GameObject> uiList = new List<GameObject>();
+
+    [System.Serializable]
+    private class LootTuple
+    {
+        [SerializeField] private GameObject loot;
+        [SerializeField] private int ratio;
+
+        public LootTuple(GameObject loot, int ratio)
+        {
+            this.loot = loot;
+            this.ratio = ratio;
+        }
+
+        public GameObject GetLoot()
+        {
+            return loot;
+        }
+
+        public int GetRatio()
+        {
+            return ratio;
+        }
+    }
+
+    [SerializeField] private LootTuple[] lootTuples;
+    [SerializeField] private float wanderRadius;
+    private Vector3 startingPosition;
+    private Vector3 roamPosition;
+    private float timer;
+    private float goingBackToStartTimer;
+    private float dist;
+    private float health;
+    private Animator animator;
+    private Camera cam;
+    private State state;
+    private Transform player;
+    private GameObject lootDropped;
+    private EnemyAttack refScript;
+    private bool canAttack = true;
+    private Renderer rend;
+    private Color[] originalColors;
+    private Color onDamageColor = Color.white;
+    private bool canMoveDuringAttack = true;
+
+    private void Awake()
+    {
+        state = State.Idle;
+        health = maxHealth;
+    }
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+        GameObject attackRadius = gameObject.transform.Find("AttackRadius").gameObject;
+        refScript = attackRadius.GetComponent<EnemyAttack>();
+        animator = GetComponentInChildren<Animator>();
+        timer = wanderTimer;
+        goingBackToStartTimer = 0;
+        SetupFlash();
+        SetupLoot();
+
+        // Make sure player exists (finished loading) before running these
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        cam = player.GetComponentInChildren<Camera>();
+        SetupHpBar();
+    }
+
+    private void SetupFlash()
+    {
+        rend = GetComponentInChildren<Renderer>();
+        originalColors = new Color[rend.materials.Length];
+        for (int i = 0; i < rend.materials.Length; i++)
+        {
+            originalColors[i] = rend.materials[i].color;
+        }
+    }
+
+    private void SetupLoot()
+    {
+        int currentWeight = 0;
+        Dictionary<GameObject, int> dropTuples = new Dictionary<GameObject, int>();
+        foreach (var loot in lootTuples)
+        {
+            currentWeight += loot.GetRatio();
+            dropTuples.Add(loot.GetLoot(), currentWeight);
+        }
+        int randomWeight = Random.Range(1, currentWeight + 1);
+        foreach (var tpl in dropTuples)
+        {
+            if (randomWeight <= tpl.Value)
+            {
+                lootDropped = tpl.Key;
+                return;
+            }
+        }
+        lootDropped = lootTuples[0].GetLoot();
+        return;
+    }
+
+    private void SetupHpBar()
+    {
+        hpBar = Instantiate(hpBar_prefab);
+        hpBarFull = hpBar.transform.Find("hpBar_full").gameObject.GetComponent<Image>();
+        SetupUI(hpBar);
+    }
+
+    private void SetupUI(GameObject ui)
+    {
+        ui.transform.SetParent(GameObject.FindObjectOfType<InventoryUI>().transform);
+        ui.transform.position = cam.WorldToScreenPoint(transform.position);
+        uiList.Add(ui);
+    }
+
+    private void Update()
+    {
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            Debug.Log(player);
+        }
+        Vector3 playerPositionWithoutYOffset = new Vector3(player.position.x, transform.position.y, player.position.z);
+        float directionVector;
+        switch (state)
+        {
+            default:
+            case State.Idle:
+                animator.SetBool("isMoving", false);
+                timer += Time.deltaTime;
+                FindTarget();
+                if (timer >= idleTimer)
+                {
+                    Vector3 newPos = RandomNavSphere(startingPosition, wanderRadius, -1);
+                    agent.SetDestination(newPos);
+                    timer = 0;
+                    state = State.Roaming;
+                    roamPosition = newPos;
+                }
+                break;
+            case State.Roaming:
+                animator.SetBool("isMoving", true);
+                timer += Time.deltaTime;
+                FindTarget();
+                Vector3 distanceToFinalPosition = transform.position - roamPosition;
+                //without this the eggplant wandering will be buggy as it may be within the Navmesh Obstacles itself
+                if (timer >= wanderTimer || distanceToFinalPosition.magnitude < 0.5f)
+                {
+                    timer = 0;
+                    state = State.Idle;
+                }
+                break;
+            case State.ChaseTarget:
+                animator.SetBool("isMoving", true);
+                directionVector = Vector3.Distance(transform.position, playerPositionWithoutYOffset);
+                if (directionVector <= agent.stoppingDistance)
+                {
+                    transform.LookAt(playerPositionWithoutYOffset);
+                    // Target within attack range
+                    state = State.AttackTarget;
+                    // Add new state to attack player
+                }
+                else
+                {
+                    agent.SetDestination(playerPositionWithoutYOffset);
+
+                }
+
+                if (Vector3.Distance(transform.position, player.position) > stopChase + 0.1f)
+                {
+                    // Too far, stop chasing
+                    state = State.GoingBackToStart;
+                }
+                break;
+            case State.AttackTarget:
+                transform.LookAt(playerPositionWithoutYOffset);
+                animator.SetBool("isMoving", false);
+                animator.ResetTrigger("attack");
+                if (canAttack == true)
+                {
+                    animator.SetTrigger("attack");
+                    canAttack = false;
+                    StartCoroutine(DelayFire());
+                }
+                directionVector = Vector3.Distance(transform.position, playerPositionWithoutYOffset);
+                if (directionVector > agent.stoppingDistance && canMoveDuringAttack)
+                {
+                    // Target within attack range
+                    state = State.ChaseTarget;
+                }
+
+                break;
+            case State.GoingBackToStart:
+                goingBackToStartTimer += Time.deltaTime;
+                animator.SetBool("isMoving", true);
+                float reachedPositionDistance = 1.0f;
+                transform.LookAt(startingPosition);
+                agent.SetDestination(startingPosition);
+                if (Vector3.Distance(transform.position, startingPosition) <= reachedPositionDistance || goingBackToStartTimer > 4.0f)
+                {
+                    // Reached Start Position
+                    animator.SetBool("isMoving", false);
+                    state = State.Idle;
+                    goingBackToStartTimer = 0;
+                }
+                break;
+        }
+
+        // Set UI to current position
+/*        Vector2 screenPos = cam.WorldToScreenPoint(transform.position);
+        if (screenPos != Vector2.zero)
+        {
+            foreach (GameObject ui in uiList)
+            {
+                if (ui != null)
+                {
+                    ui.transform.position = screenPos;
+                }
+                else
+                {
+                    uiList.Remove(null);
+                }
+            }
+        }*/
+
+
+    }
+
+    private IEnumerator DelayFire()
+    {
+        yield return new WaitForSeconds(timeBetweenAttacks);
+        canAttack = true;
+    }
+
+    private void FindTarget()
+    {
+        dist = Vector3.Distance(player.position, transform.position);
+
+        if (dist <= distanceTriggered)
+        {
+            timer = 0;
+            state = State.ChaseTarget;
+
+            SetupUI(Instantiate(enemyAlert_prefab));
+        }
+    }
+
+    public void HandleHit(float damage)
+    {
+        this.health -= damage;
+        hpBarFull.fillAmount = health / maxHealth;
+        StartCoroutine(FlashOnDamage());
+        SpawnDamageCounter(damage);
+
+        if (this.health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void SpawnDamageCounter(float damage)
+    {
+        GameObject damageCounter = Instantiate(damageCounter_prefab);
+        damageCounter.transform.GetComponentInChildren<Text>().text = damage.ToString();
+        SetupUI(damageCounter);
+    }
+
+    private void Die()
+    {
+        DropLoot();
+        Destroy(hpBar);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator FlashOnDamage()
+    {
+        for (var i = 0; i < rend.materials.Length; i++)
+        {
+            rend.materials[i].color = onDamageColor;
+        }
+
+        float duration = 0.1f;
+        while (duration > 0)
+        {
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+
+        for (var i = 0; i < rend.materials.Length; i++)
+        {
+            rend.materials[i].color = originalColors[i];
+        }
+    }
+
+    private void DropLoot()
+    {
+        Instantiate(lootDropped, transform.position, Quaternion.identity);
+    }
+
+    private Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector2 randPos = Random.insideUnitCircle * dist;
+        Vector3 randDirection = new Vector3(randPos.x, transform.position.y, randPos.y);
+        while ((randDirection - origin).magnitude < 5.0f)
+        {
+            randPos = Random.insideUnitCircle * dist;
+            randDirection = new Vector3(randPos.x, transform.position.y, randPos.y);
+        }
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+    public void attackPlayerStart()
+    {
+        canMoveDuringAttack = false;
+        refScript.attackPlayerStart();
+    }
+
+    public void attackPlayerDealDamage()
+    {
+        refScript.attackPlayerDealDamage();
+    }
+
+
+    public void attackPlayerEnd()
+    {
+        canMoveDuringAttack = true;
+        refScript.attackPlayerEnd();
+    }
+}
