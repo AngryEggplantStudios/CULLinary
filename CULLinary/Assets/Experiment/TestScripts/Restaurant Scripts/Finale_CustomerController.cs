9259025d531862ea34b664012d2c366048b8ca33
geezlouisee <louisehl.lee@gmail.com>
2021-04-05 22:15:55 +0800

    Added Finale Scene (in Restaurant)

---
+++ CULLinary/Assets/Experiment/TestScripts/Restaurant Scripts/Finale_CustomerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Finale_CustomerController : MonoBehaviour
+{
+ 
+    private void Start()
+    {
+        // Play the sitting down animation  
+        Animator animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("SitDown", true);
+        Debug.Log("animator is: " + animator.ToString());
+
+        
+    }
+
+    
+
+}
