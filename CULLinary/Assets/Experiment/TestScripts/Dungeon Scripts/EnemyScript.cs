9e12c33766ea8778e1ace5e2b5b6e56d53f6d89e
WhiteLio <lio.lim@yahoo.com>
2021-04-08 21:20:36 +0800

    Alert enemy on damage
    To counter ranged attack sniping

--- CULLinary/Assets/Experiment/TestScripts/Dungeon Scripts/EnemyScript.cs
+++ CULLinary/Assets/Experiment/TestScripts/Dungeon Scripts/EnemyScript.cs
@@ -270,17 +270,27 @@ public class EnemyScript : Enemy
         dist = Vector3.Distance(player.position, transform.position);
         if (dist <= distanceTriggered)
         {
-            timer = 0;
-            state = State.ChaseTarget;
-            
-            SetupUI(Instantiate(enemyAlert_prefab));
-            audioSourceAttack.clip = alertSound;
-            audioSourceAttack.Play();
+            Alert();
         }
+    }
+
+    private void Alert()
+    {
+        timer = 0;
+        state = State.ChaseTarget;
+        
+        SetupUI(Instantiate(enemyAlert_prefab));
+        audioSourceAttack.clip = alertSound;
+        audioSourceAttack.Play();
     }
 
     public override void HandleHit(float damage)
     {
+        if (state == State.Idle || state == State.Roaming)
+        {
+            Alert();
+        }
+        
         this.health -= damage;
         hpBarFull.fillAmount = health/maxHealth;
         StartCoroutine(FlashOnDamage());
