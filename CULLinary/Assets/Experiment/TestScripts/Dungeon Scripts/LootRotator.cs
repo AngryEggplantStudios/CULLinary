39aee2ebd028c9963c06775a438164dcfb907e72
WhiteLio <lio.lim@yahoo.com>
2021-04-10 01:50:21 +0800

    Make loot more obvious
    Make loot bob up and down
    Increase rotation speed
    Add light

--- CULLinary/Assets/Experiment/TestScripts/Dungeon Scripts/LootRotator.cs
+++ CULLinary/Assets/Experiment/TestScripts/Dungeon Scripts/LootRotator.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class LootRotator : MonoBehaviour
 {
+    public float amplitude = 0.05f;
+    public float verticalSpeed = 2f;
+    public float rotationSpeed = 90;
+
     private void Update()
     {
-        transform.Rotate(new Vector3(10, 45, 10) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
+
+        Vector3 pos = transform.position;
+        float newY = (Mathf.Sin(Time.time * verticalSpeed) + 1) * amplitude;
+        transform.position = new Vector3(pos.x, newY, pos.z);
     }
 }
