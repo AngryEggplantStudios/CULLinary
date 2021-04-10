7ee73d44ebdbe708fad46c1ccb253daefa2767e0
alcen <49450743+alcen@users.noreply.github.com>
2021-04-10 18:33:25 +0800

    Add dialogue for boss portal
    * Fix Tut_boss still having squishing sounds after fake death

--- CULLinary/Assets/BossPortal.cs
+++ CULLinary/Assets/BossPortal.cs
@@ -7,22 +7,53 @@ public class BossPortal : MonoBehaviour
 {
     [SerializeField] private SaveGameDataSystem saveGameDataSystem;
     [SerializeField] private SceneIndexes bossScene;
+    // This field will be assigned by the map generator
+    public DialogueLoader dialogueLoader = null;
     
     private bool isTriggered;
+
+    private void TransitionToBossScene()
+    {
+        if (saveGameDataSystem != null) {
+            saveGameDataSystem.SaveGameData((int)SceneIndexes.DUNGEON);
+        }
+        Debug.Log("Entering boss");
+        SceneManager.LoadScene((int)bossScene);
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F) && isTriggered)
         {
-            // should save current health too
-            Debug.Log("Entering boss");
-            saveGameDataSystem.SaveGameData((int)SceneIndexes.DUNGEON);
-            LoadScene();
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (dialogueLoader == null || player == null) {
+                Debug.Log("Dialogue loader or player not found");
+                // should save current health too
+                TransitionToBossScene();
+            } else {
+                DungeonPlayerController dpc = player.GetComponent<DungeonPlayerController>();
+                DungeonPlayerAim dpa = player.GetComponent<DungeonPlayerAim>();
+                DungeonPlayerSlash dps = player.GetComponent<DungeonPlayerSlash>();
+                if (dpc == null || dpa == null || dps == null) {
+                    Debug.Log("Player controllers not found. Will not disable player controls");
+                } else {
+                    dpc.DisableMovement();
+                    dpa.disableMovement();
+                    dps.disableMovement();  // Re-enabled implictly by loading new scene
+                }
+                Animator playerAnim = player.GetComponent<Animator>();
+                if (playerAnim == null) {
+                    Debug.Log("Player animator not found. May get stuck in running animation");
+                } else {
+                    playerAnim.SetFloat("Speed", 0.0f);
+                }
+                Dialogue enterPortalDialogue = DialogueParser.Parse(
+                    "{[L]0}It smells like fried potatoes and chemicals!" +
+                    "Well... here goes nothing.");
+                dialogueLoader.LoadAndRunWithoutCustomer(enterPortalDialogue);
+                dialogueLoader.SetDialogueEndCallback(() => TransitionToBossScene());
+            }
         }
-    }
-    private void LoadScene()
-    {
-        SceneManager.LoadScene((int)bossScene);
     }
 
     private void OnTriggerStay(Collider collider)
